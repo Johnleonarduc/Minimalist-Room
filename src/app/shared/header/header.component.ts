c0bab5a52b05e9cc3106e6444fbc9c7de6df9942
Johnleonarduc <johnleonarduc@gmail.com>
2023-04-13 15:31:59 +0100

    complete header component

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,44 @@
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.mobile.css', './header.component.desktop.css']
+})
+export class HeaderComponent {
+
+  constructor() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  navLinks = [
+    {route: 'home', title: 'home'},
+    {route: 'about', title: 'about us'},
+    {route: 'services', title: 'services'},
+    {route: 'teams', title: 'our teams'},
+  ]
+
+  faBars = faBars;
+  faXmark = faXmark;
+  screenWidth: number;
+  isMenuBtnClicked: boolean = false;
+
+  // HostListener to detect screen size changes
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+  }
+
+
+  toggleNav(nav:  HTMLElement){
+    if(nav.classList.contains('show-mobile')){
+      nav.classList.remove('show-mobile');
+      nav.classList.add('hide');
+    }else{
+      nav.classList.remove('hide');
+      nav.classList.add('show-mobile');
+    }
+    this.isMenuBtnClicked = !this.isMenuBtnClicked;
+  }
+}
