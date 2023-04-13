188172e91b81583f84c2d88965133055a0fefeb0
Johnleonarduc <johnleonarduc@gmail.com>
2023-04-13 15:30:34 +0100

    complete header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CallToActionComponent } from './shared/call-to-action/call-to-action.component';
+import { HomeComponent } from './home/home.component';
+import { ImageGridComponent } from './shared/image-grid/image-grid.component';
+import { ContactButtonComponent } from './shared/contact-button/contact-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CallToActionComponent,
+    HomeComponent,
+    ImageGridComponent,
+    ContactButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
