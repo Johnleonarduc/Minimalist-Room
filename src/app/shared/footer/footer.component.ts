838384a6eae34b5fb48a2dae9a339aca067abae3
Johnleonarduc <johnleonarduc@gmail.com>
2023-04-14 01:59:43 +0100

    complete basic structure and styles, less final touches

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { faFacebook, faInstagram, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.mobile.css', './footer.component.desktop.css']
 })
 export class FooterComponent {
+
+  socialIcons = [
+    { link: 'https://www.facebook.com', icon: faFacebook},
+    { link: 'https://www.instagram.com', icon: faInstagram},
+    { link: 'https://www.tiktok.com', icon: faTiktok},
+    { link: 'https://www.youtube.com', icon: faYoutube},
+  ]
 
 }
--- src/app/shared/image-grid/image-grid.component.ts
+++ src/app/shared/image-grid/image-grid.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-grid.component.css']
 })
 export class ImageGridComponent {
+  images = [
+    { small: '../../../assets/small/image1.jpg', medium: '../../../assets/medium/image1.jpg', large: '../../../assets/medium/image1.jpg', alt:'image of chair and basket'},
+    { small: '../../../assets/small/image2.jpg', medium: '../../../assets/medium/image2.jpg', large: '../../../assets/medium/image2.jpg', alt:'image of table, phone, and mirror'},
+    { small: '../../../assets/small/image3.jpg', medium: '../../../assets/medium/image3.jpg', large: '../../../assets/large/image3.jpg', alt:'image of couch and cabinet'},
+    { small: '../../../assets/small/image4.jpg', medium: '../../../assets/medium/image4.jpg', large: '../../../assets/medium/image4.jpg', alt:'image of couch and side table with lamp'},
+    { small: '../../../assets/small/image5.jpg', medium: '../../../assets/medium/image5.jpg', large: '../../../assets/medium/image5.jpg', alt:'image of cemppty room'},
 
+  ]
 }
